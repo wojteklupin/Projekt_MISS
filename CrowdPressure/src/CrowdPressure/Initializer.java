c22fcd02ee7dd566f76e9b74e171e96fd4d26d48
jakubzol <jakubzol@student.agh.edu.pl>
2019-03-31 23:03:52 +0200

    Huge commit

---
+++ CrowdPressure/src/CrowdPressure/Initializer.java
@@ -0,0 +1,18 @@
+package CrowdPressure;
+
+import CrowdPressure.model.Board;
+
+public class Initializer {
+
+    static public Engine createEngine(MapBuilder builder, String simulationName) {
+
+
+        //Board simulationMap = builder.buildCustomSimulation(simulationName);
+
+        Board simulationMap = MapBuilder.buildDefaultSimulation();
+
+
+        return new Engine(simulationMap);
+    }
+
+}
